50033ccea6bf7a9ab169a08ca29fe65710316858
shvetson <shvetson@gmail.com>
2023-03-28 15:48:55 +0300

    add request parameters

--- src/main/kotlin/ru/shvets/route/DefaultRoute.kt
+++ src/main/kotlin/ru/shvets/route/DefaultRoute.kt
@@ -1,6 +1,7 @@
 package ru.shvets.route
 
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -14,6 +15,14 @@ fun Route.default() {
 
     route("") {
         get("/") {
+            println("URI: ${call.request.uri}")
+            println("Headers: ${call.request.headers.names()}")
+            println("User-Agent: ${call.request.headers["User-Agent"]}")
+            println("Accept: ${call.request.headers["Accept"]}")
+            println("Query Parameters: ${call.request.queryParameters.names()}")
+            println("Name: ${call.request.queryParameters["name"]}")
+            println("Email: ${call.request.queryParameters["email"]}")
+
             call.respondText("Hello Ktor!")
         }
     }
